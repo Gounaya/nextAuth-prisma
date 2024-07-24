c7e533b14933fcd753708069c8e040036632c312
GOUNAYA Oussama <b179ry@login.axa>
2024-07-24 16:07:31 +0200

    setup google auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,12 +3,19 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 const githubId = process.env.GITHUB_ID;
 const githubSecret = process.env.GITHUB_SECRET;
 
+const googleId = process.env.GOOGLE_ID;
+const googleSecret = process.env.GOOGLE_SECRET;
+
 if (!githubId || !githubSecret) {
   throw new Error("❌ Missing GITHUB_ID or GITHUB_SECRET in NextAuth config");
+} else if (!googleId || !googleSecret) {
+  throw new Error("❌ Missing GOOGLE_ID or GOOGLE_SECRET in NextAuth config");
+
 }
 
 export const authOptions  = {
@@ -16,7 +23,12 @@ export const authOptions  = {
     GithubProvider({
       clientId: githubId,
       clientSecret: githubSecret
+    }),
+    GoogleProvider({
+      clientId: googleId,
+      clientSecret: googleSecret,
     })
+
   ],
   callbacks: {
     session: async ({session, user}: any) => {
